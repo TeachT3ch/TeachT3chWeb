13855559b31c65af8c78fb074ad64109df7d7d96
Nany Thery <nadinethery@gmail.com>
2020-07-01 21:06:14 +0200

    update resources, and update temario with slides of t1

---
+++ recursos.js
@@ -0,0 +1,119 @@
+var recursos = [
+    ,
+    { category: "Documentación y Sintaxis 📄",
+        items :[
+        {   
+            Name: "MDN Web Docs (Español)",
+            Description: "Mozilla ha recopilado esta guía muy desarrollada de conceptos de programación y diseño web. Aquí podrás encontrar información básica sobre sintaxis y definiciones para Javascript, HTML y CSS entre otras cosas.",
+            Link: "https://developer.mozilla.org/es/"
+        },
+        {  
+            Name: "W3Schools (Inglés)",
+            Description: "Esta web recopila sintaxis y definiciones con ejercicios interactivos para afianzar conceptos.",
+            Link: "https://www.w3schools.com/"
+            }
+        ]
+    },
+    { category: "Cursos Gratuitos en español 👩‍🏫🇪🇸 ",
+        items :[
+        {   
+            Name: "Javascript desde cero",
+            Description: "Píldoras Informáticas nos enseña los conceptos básicos de JS desde 0.",
+            Link: "https://www.pildorasinformaticas.es/course/javascript-desde-0/"
+        },
+        {  
+            Name: "Curso de Javascript ",
+            Description: "Desde su canal, Ada Lovecode, nos enseñará en vídeos cortos y claros a programar en JS.",
+            Link: "https://www.youtube.com/playlist?list=PLI7nHlOIIPOJtTDs1HVJABswW-xJcA7_o"
+            },
+            {  
+                Name: "Javascript para Principiantes",
+                Description: "Fazt explica en un único vídeo los conceptos básicos para programar en JS",
+                Link: "https://www.youtube.com/watch?v=RqQ1d1qEWlE"
+                }
+        ]
+    }, 
+    { category: "Plataformas de cursos gratuitos en inglés 👨‍🏫🇬🇧", 
+    items:
+        [
+            {
+                Name: "CodeCademy.com",
+                Description: "Plataforma de ejercicios interactivos.",
+                Link: "https://www.CodeCademy.com"
+            },
+            {
+                Name: "FreeCodeCamp.com",
+                Description: "Cursos con ejercicios interactivos y proyectos prácticos que te permitirán obtener una certificación final ¡gratis!",
+                Link: "https://www.freecodecamp.com"
+            }
+        ]
+    },
+    { category: "CSS 💄", 
+    items:
+        [
+            {
+                Name: "LenguajeCSS (español)",
+                Description: "Compendio de artículos sobre CSS y HTML.",
+                Link: "https://www.lenguajeCSS.com"
+            },
+            {
+                Name: "CSS Tricks",
+                Description: "Aprende y actualízate a diario para dominar el CSS como nadie con esta web.",
+                Link: "https://www.css-tricks.com"
+            }
+        ]
+    },
+    { category: "Para mantenerte al día y meterte en el mundillo 😎", 
+    items:
+        [
+            {
+                Name: "EdTeam",
+                Description: "Canal de Youtube. podrás aprender día a día sobre conceptos y términos de moda en la tecnología explicados de manera muy sencilla.",
+                Link: "https://www.youtube.com/user/escueladigitalperu/videos"
+            },
+            {
+                Name: "Platzi",
+                Description: "Canal de Youtube. Conferencias, contenido variado relacionado con la tecnología y la actualidad.",
+                Link: "https://www.youtube.com/user/mejorandolaweb"
+            },
+            {
+                Name: "El Blog de Manz",
+                Description: "Un poco de todo en tecnología",
+                Link: "https://www.emezeta.com/"
+            },
+            {
+                Name: "Didacticode y Ada Lovecode",
+                Description: "Blog con desafíos y ejercicios resueltos de programación",
+                Link: "https://www.emezeta.com/"
+            },
+            {
+                Name: "The Coding Love",
+                Description: "Un poco de humor de desarrolladores con grandes dosis de realidad",
+                Link: "https://thecodinglove.com/"
+            },
+            {
+                Name: "Dev.to",
+                Description: "Una red social mundial para desarrolladores de todos los niveles (sobre todo en inglés). Para compartir conocimientos y experiencias.",
+                Link: "https://dev.to/"
+            },
+
+        ]
+    },
+    { category: "Y si todo falla y Google no tiene la respuesta...🆘 ", 
+    items:
+        [
+            {
+                Name: "StackOverFlow",
+                Description: "Foro de preguntas y respuestas. Normalmente, Google te sugerirá las preguntas que son similares a la tuya ya en el buscador. Pero si no lo encuentras, puedes preguntar. Cuidado, StackOverFlow tiene normas muy estrictas sobre cómo preguntar y qué, no olvides mirar las reglas antes :D",
+                Link: "https://stackoverflow.com/"
+            },
+            {
+                Name: "Disboard.org",
+                Description: "Muchos lenguajes tienen canales de Discord abiertos donde la comunidad podrá ayudarte en directo. Utiliza Disboard para encontrar un servidor si no tienes claro a cuál",
+                Link: "https://disboard.org/es"
+            },
+
+        ]
+    }
+
+] 
